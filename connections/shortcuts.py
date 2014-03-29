833c1f34028f7c67d2e0f76f5f357c0d26ca1ff1
Akis Kesoglou <akiskesoglou@gmail.com>
2014-03-30 00:50:37 +0200

    Removed redundant inner imports

--- connections/shortcuts.py
+++ connections/shortcuts.py
@@ -10,35 +10,28 @@ def get_relationship(name):
 
 
 def get_connection(relationship, from_obj, to_obj):
-    from .models import get_relationship
     return get_relationship(relationship).get_connection(from_obj, to_obj)
 
 
 def create_connection(relationship, from_obj, to_obj):
-    from .models import get_relationship
     return get_relationship(relationship).create_connection(from_obj, to_obj)
 
 
 def connection_exists(relationship, from_obj, to_obj):
-    from .models import get_relationship
     return get_relationship(relationship).connection_exists(from_obj, to_obj)
 
 
 def connections_from_object(relationship, from_obj):
-    from .models import get_relationship
     return get_relationship(relationship).connections_from_object(from_obj)
 
 
 def connections_to_object(relationship, to_obj):
-    from .models import get_relationship
     return get_relationship(relationship).connections_to_object(to_obj)
 
 
 def connected_objects(relationship, from_obj):
-    from .models import get_relationship
     return get_relationship(relationship).connected_objects(from_obj)
 
 
 def connected_to_objects(relationship, to_obj):
-    from .models import get_relationship
     return get_relationship(relationship).connected_to_objects(to_obj)
